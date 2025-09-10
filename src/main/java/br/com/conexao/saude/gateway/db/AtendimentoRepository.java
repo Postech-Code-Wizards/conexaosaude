58c2ac015dbda864424d2bd06c65379ca5c84e31
andy-spch <anderson.donaruma@gmail.com>
2025-09-10 16:54:15 -0300

    (fix-CS-005) - fixed repository and jdbc connection

--- src/main/java/br/com/conexao/saude/gateway/db/AtendimentoRepository.java
+++ src/main/java/br/com/conexao/saude/gateway/db/AtendimentoRepository.java
@@ -1,6 +1,7 @@
 package br.com.conexao.saude.gateway.db;
 
+import br.com.conexao.saude.gateway.db.jpa.AtendimentoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AtendimentoRepository extends JpaRepository<AtendimentoRepository, Long> {
+public interface AtendimentoRepository extends JpaRepository<AtendimentoEntity, Long> {
 }
--- src/main/java/br/com/conexao/saude/gateway/db/ExameRepository.java
+++ src/main/java/br/com/conexao/saude/gateway/db/ExameRepository.java
@@ -1,6 +1,7 @@
 package br.com.conexao.saude.gateway.db;
 
+import br.com.conexao.saude.gateway.db.jpa.ExameEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExameRepository extends JpaRepository<ExameRepository, Long> {
+public interface ExameRepository extends JpaRepository<ExameEntity, Long> {
 }
